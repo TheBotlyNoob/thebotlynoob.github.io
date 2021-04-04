85a654724ed7d8ddeb6ab2429932f1bf927cb154
Jay Jackson <72417807+TheBotlyNoob@users.noreply.github.com>
2021-04-04 12:25:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Jay Jackson</title>
+    <link href="css/index.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+      <img class="githubStats" src="https://raw.githubusercontent.com/TheBotlyNoob/github-stats/master/generated/languages.svg"></img>
+      <img class="githubStats" src="https://raw.githubusercontent.com/TheBotlyNoob/github-stats/master/generated/overview.svg"></img>
+    <script src="js/index.js"></script>
+  </body>
+</html>
