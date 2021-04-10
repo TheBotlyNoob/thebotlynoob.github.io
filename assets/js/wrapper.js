82e7f76c48fd8c59e8fe772387764d2868d79425
Jay Jackson <>
2021-04-10 15:05:40 +0000

    i know the "fa" class id depreciated but its still the easiest way

--- assets/js/wrapper.js
+++ assets/js/wrapper.js
@@ -1,3 +1,3 @@
 $(".middle").wrap("<div class='container'></div>");
 $("code").wrap("<pre></pre>");
-$("[class$='-icon']").each(function(){var a=$(this).attr("class").split(" ").pop().replace("-icon","");$(this).prepend(`<i class="icon fas fa-${a}"></i>`)});
+$("[class$='-icon']").each(function(){var a=$(this).attr("class").split(" ").pop().replace("-icon","");$(this).prepend(`<i class="icon fa fa-${a}"></i>`)});
