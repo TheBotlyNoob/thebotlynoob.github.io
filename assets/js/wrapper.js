d54b72d143c1d014fd215d161004e0ca143b8334
Jay Jackson <>
2021-04-10 04:14:24 +0000

    better!

--- assets/js/wrapper.js
+++ assets/js/wrapper.js
@@ -1,3 +1,3 @@
 $(".middle").wrap("<div class='container'></div>");
 $("code").wrap("<pre></pre>");
-$("[class$='-icon']").each(function(){var a=$(this).attr("class").split(" ").pop().replace("-icon","");$("[class$='-icon']").prepend(`<img src="/assets/icons/${a}.svg"></img>`)});
+$("[class$='-icon']").each(function(){var a=$(this).attr("class").split(" ").pop().replace("-icon","");$(this).prepend(`<i class="icon fas fa-${a}"></i>`)});
