f8b21110808cad6a3bd2187c596b9c5303c7ddd0
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-17 22:19:16 -0400

    kkkk

--- minifyFiles.js
+++ minifyFiles.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs"),
         babel = require("@babel/core"),
-        jsmini = require("uglify-js").minify,
+        jsmini = require("javascript-minifier").minify,
         cssmini = require("csso").minify,
         htmlmini = require("html-crush").crush;
 
@@ -10,15 +10,10 @@ const js = babel.transformFileSync("assets/js/script.js").code,
         css = fs.readFileSync("assets/css/styles.css").toString(),
         html = fs.readFileSync("index.dev.html").toString();
 
-const jsMinified = jsmini(js).code,
-      cssMinified = cssmini(css).css,
+const cssMinified = cssmini(css).css,
       htmlMinified = htmlmini(html).result;
-console.log(jsMinified);
-console.log("----");
-console.log(cssMinified);
-console.log("----");
-console.log(htmlMinified);
 
-fs.writeFileSync("assets/js/script.min.js", jsMinified);
+jsmini(js).then(code => fs.writeFileSync("assets/js/script.min.js", code))
+
 fs.writeFileSync("assets/css/styles.css", cssMinified);
 fs.writeFileSync("index.html", htmlMinified);
