adcba48f47cbfdd36144d92cb4911d90317b2a98
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-18 14:50:09 -0400

    umm more testing

--- minifyFiles.js
+++ minifyFiles.js
@@ -1,5 +1,18 @@
 "use strict";
 
+const htmlminiOpts = {
+  "removeRedundantAttributes": true,
+  "removeOptionalTags": true,
+  "removeComments": true,
+  "quoteCharacter": '"',
+  "caseSensitive": true,
+  "collapseWhitespace": true,
+  "continueOnParseError": true,
+  "minifyCSS": true,
+  "minifyJS": true,
+  "minifyURLs": true,
+}
+
 const fs = require("fs"),
         babel = require("@babel/core"),
         jsmini = require("uglify-js").minify,
@@ -10,9 +23,9 @@ const js = babel.transformFileSync("assets/js/script.js").code,
         css = fs.readFileSync("assets/css/styles.css").toString(),
         html = fs.readFileSync("index.dev.html").toString();
 
-const jsMinified = jsmini(js).code.toString(),
+const jsMinified = jsmini(js, htmlminiOpts).code.toString(),
       cssMinified = cssmini(css).css.toString(),
-      htmlMinified = htmlmini(html).result.toString();
+      htmlMinified = htmlmini(html).toString();
 
 fs.writeFileSync("assets/js/script.min.js", jsMinified);
 console.log("Minified JavaScript Saved!");
