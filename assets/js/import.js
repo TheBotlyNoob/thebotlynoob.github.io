1df5333d49772866222aa9a2068a2aa0e342e7db
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-18 19:40:43 -0400

    testing

---
+++ assets/js/import.js
@@ -0,0 +1,2 @@
+import {} from "https://kit.fontawesome.com/9ec0bb60ff.js";import {jQuery} from "https://unpkg.com/jquery/dist/jquery.min.js";import {} from "https://unpkg.com/prismjs/components/prism-core.min.js";import {} from "https://unpkg.com/prismjs/plugins/autoloader/prism-autoloader.min.js";
+window.$ = window.jQuery = jQuery;
