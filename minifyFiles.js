f9503c5df4b1d3649746375e3455b1a59ef59ba4
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-17 22:03:45 -0400

    try again

--- minifyFiles.js
+++ minifyFiles.js
@@ -4,7 +4,7 @@ const fs = require("fs"),
         babel = require("@babel/core"),
         jsmini = require("uglify-js").minify,
         cssmini = require("csso").minify,
-        htmlmini = require('html-minifier').minify;
+        htmlmini = require("html-crush").crush;
 
 const js = babel.transformFileSync("assets/js/script.js").code,
         css = fs.readFileSync("assets/css/styles.css"),
@@ -12,7 +12,7 @@ const js = babel.transformFileSync("assets/js/script.js").code,
 
 const jsMinified = jsmini(js).code,
       cssMinified = cssmini(css).css,
-      htmlMinified = htmlmini(html);
+      htmlMinified = htmlmini(html).result;
 
 fs.writeFileSync("assets/js/script.min.js", jsMinified);
 fs.writeFileSync("assets/css/styles.css", cssMinified);
