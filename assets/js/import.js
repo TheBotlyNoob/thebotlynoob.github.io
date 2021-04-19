ad3bb615c48bc16f9cd208cf6121fecd4b21f353
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-18 21:02:55 -0400

    hmm

--- assets/js/import.js
+++ assets/js/import.js
@@ -1,2 +1,2 @@
-import {} from "https://kit.fontawesome.com/9ec0bb60ff.js";import {jQuery} from "https://unpkg.com/jquery/dist/jquery.min.js";import {} from "https://unpkg.com/prismjs/components/prism-core.min.js";import {} from "https://unpkg.com/prismjs/plugins/autoloader/prism-autoloader.min.js";
+import * as fontAwesome from "https://kit.fontawesome.com/9ec0bb60ff.js";import * as jQuery from "https://unpkg.com/jquery/dist/jquery.min.js";import * as prism from "https://unpkg.com/prismjs/components/prism-core.min.js";import * as prismAutoloader from "https://unpkg.com/prismjs/plugins/autoloader/prism-autoloader.min.js";
 window.$ = window.jQuery = jQuery;
