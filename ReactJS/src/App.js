b9b04f32d3334e2afe0f6268cb9e07e7bd173edf
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-20 16:57:54 -0400

    hmm

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -23,7 +23,7 @@ function App() {
           </Button>
         </a>
         <br />
-        <Get src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />
+        <Get style={{color: 'white'}} src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />
       </header>
     </div>
   );
