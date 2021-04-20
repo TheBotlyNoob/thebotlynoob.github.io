003ff9a087d45a2a6f8e7d07d46d56e4e34a5f73
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-20 16:32:50 -0400

    doing better!

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
 import Get from './get';
+import './bootstrap.min.css';
+import { Button } from 'reactstrap';
+import './App.css';
 
 function App() {
   return (
@@ -16,8 +18,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button color="primary">
+            Learn React
+          </Button>
         </a>
+        <br />
         <Get src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />
       </header>
     </div>
