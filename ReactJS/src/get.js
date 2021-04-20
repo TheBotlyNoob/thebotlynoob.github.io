b1aeb1755fa2f33a95b5decc072dd80f2ee82c39
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-20 13:32:27 -0400

    ok... (again)

--- ReactJS/src/get.js
+++ ReactJS/src/get.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class getJSON extends React.Component {
+export default class getJSON extends Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -38,7 +38,7 @@ class getJSON extends React.Component {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
         return <div>Loading...</div>;
-      } else if (!JSON.parse(items) == "object") {
+      } else if (!JSON.parse(items) === Object) {
         return (new Error("The URL Does Not Return A JSON Object. Try Using The get Element!"));
       } else {
         return (
