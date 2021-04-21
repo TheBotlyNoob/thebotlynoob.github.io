6d56776440c85cb4e164669c6ad8fd2fa38607f8
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-20 21:23:21 -0400

    discord widget!

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import Get from './get';
 import './bootstrap.min.css';
-import { Button } from 'reactstrap';
+import {Button} from 'reactstrap';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,7 +23,11 @@ function App() {
           </Button>
         </a>
         <br />
-        <Get style={{color: 'white'}} src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />
+        <b><u>Bee Movie Script</u></b>
+        <br />
+        <code id="beeMovieScript">
+                <Get src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />
+        </code>
       </header>
     </div>
   );
