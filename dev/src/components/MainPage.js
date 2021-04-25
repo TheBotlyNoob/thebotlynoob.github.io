571e18ba6d81980c2576f82f9e9af25f3849c447
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-24 21:42:00 -0400

    It works! (kinda)

--- dev/src/components/MainPage.js
+++ dev/src/components/MainPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Cards from 'react-ui-cards';
 import Button from 'react-bootstrap/Button';
 import logo from './logo.svg';
 import Get from './Get';
+import './bootstrap.min.css';
 import './App.css'
-//import './bootstrap.min.css';
 
 const vars = {};
 
@@ -25,36 +24,6 @@ export default function Main () {
             Learn React
           </Button>
         </a>
-            <br />
-            <b><u>Bee Movie Script</u></b>
-            <br />
-            <span style={{'display': 'none'}}>{vars.beeMovieScript = <Get src="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script" />}</span>
-            <code id="beeMovieScript" style={{'whitespace': 'pre'}}>
-                    <pre style={{'color': 'inherit'}}>{vars.beeMovieScript}</pre>
-            </code>
-            <footer style={{'backgroundColor': 'white'}}>
-                <Cards.UserCard className="d-flex justify-content-center"
-                    float
-                    header='https://i.imgur.com/w5tX1Pn.jpg'
-                    avatar='https://i.imgur.com/uDYejhJ.jpg'
-                    name='Jay Jackson'
-                    positionName='Software Developer'
-                    stats={[
-                        {
-                            name: 'followers',
-                            value: 21
-                        },
-                        {
-                            name: 'following',
-                            value: 37
-                        },
-                        {
-                            name: 'posts',
-                            value: 117
-                        }
-                    ]}
-                />
-            </footer>
     </div>
 );
 }
--- dev/src/index.js
+++ dev/src/index.js
@@ -1,9 +1,27 @@
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import routes from './routes';
+import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import GitHubPage from './components/GitHub';
+import YouTubePage from './components/YouTube';
+import NotFoundPage from './components/404';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <Router routes={routes} history={createBrowserHistory()} />,
+  <Router>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route path="/GitHub">
+          <GitHubPage />
+        </Route>
+        <Route path="/YouTube">
+          <YouTubePage />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+  </Router>,
   document.getElementById('root')
 );
