f4f972947109e4e8396d1e5f0d67412ec5225999
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-24 23:00:35 -0400

    ok...

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import GitHubPage from "./components/GitHub";
+import YouTubePage from "./components/YouTube";
+import NotFoundPage from "./components/404";
 import ReactDOM from "react-dom";
 
 ReactDOM.render (
   <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/GitHub" component={GitHubPage} />
-      <Route path="/YouTube" component={YouTubePage} />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/GitHub" component={GitHubPage} />
+        <Route path="/YouTube" component={YouTubePage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
   </Router>,
   document.getElementById ("root")
 );
