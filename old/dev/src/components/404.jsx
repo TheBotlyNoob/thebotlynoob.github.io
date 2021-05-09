04d556b5462e686e2ac83087582d8265e64a7921
Jay Jackson <72417807+TheBotlyNoob@users.noreply.github.com>
2021-05-09 16:50:49 -0400

    Delete old directory

--- old/dev/src/components/404.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound () {
-  return (
-    <code style={{"color": "lightblue"}}>Woah There, You Seem To Be Lost!
-      <br />
-    </code>
-  );
-}
--- old/dev/src/components/YouTube.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function YouTube () {
-  return (
-    <code>The YouTube Page Is A WIP</code>
-  );
-}
