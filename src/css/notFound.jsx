f29599549f180b744df2a65fc26b1328a85c585c
TheBotlyNoob <thebotlynoob@gmail.com>
2021-05-19 17:05:00 -0400

    fix html

--- src/css/notFound.jsx
+++ src/css/notFound.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 export default function notFound () {
     return (
         <style>{`
+        #run {
+            position: absolute;
+            width: 1px;
+            height: 1px; 
+            margin: -1px; 
+            border: 0; 
+            padding: 0; 
+            clip: rect(0 0 0 0); 
+            overflow: hidden;
+        }
+
         *, :after, :before, html {
             box-sizing: border-box;
         }
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ import { notFound as Styles } from '../css';
 export default class notFound extends React.Component {
   constructor (props) {
     super(props);
-    
+
     this.state = {
       keyPressed: false
     };
@@ -27,20 +27,20 @@ export default class notFound extends React.Component {
       <div>
         <Styles />
         <div className='notfound'>
-            <div className="centered"><span className="inverted">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;</div>
-            <div className="centered"><span className="inverted">&nbsp;4&nbsp;0&nbsp;4&nbsp;</span><span className="shadow">&nbsp;</span></div>
-            <div className="centered"><span className="inverted">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span className="shadow">&nbsp;</span></div>
-            <div className="centered">&nbsp;<span className="shadow">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
-            <div className="row">&nbsp;</div>
-            <div className="row">A fatal exception 404 has occurred at C0DE:ABAD1DEA in 0xC0DEBA5E.</div>
-            <div className="row">The current request will be terminated.</div>
-            <div className="row">&nbsp;</div>
-            <div className="row">* Press any key to return to the previous page.</div>
-            <div className="row">* Press CTRL+ALT+DEL to restart your computer. You will</div>
-            <div className="row">&nbsp;&nbsp;lose any unsaved information in all applications.</div>
-            <div className="row">&nbsp;</div>
-            <div className="centered">Press any key to continue...<span className="blink">&#9608;</span></div>
-            <input style={{position: 'absolute', width: '1px', height: '1px', margin: '-1px', border: '0', padding: '0', clip: 'rect(0 0 0 0)', overflow: 'hidden'}} autoFocus onKeyPress={() => this.run()}></input>
+            <div className='centered'><span className='inverted'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;</div>
+            <div className='centered'><span className='inverted'>&nbsp;4&nbsp;0&nbsp;4&nbsp;</span><span className='shadow'>&nbsp;</span></div>
+            <div className='centered'><span className='inverted'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span className='shadow'>&nbsp;</span></div>
+            <div className='centered'>&nbsp;<span className='shadow'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
+            <div className='row'>&nbsp;</div>
+            <div className='row'>A fatal exception 404 has occurred at C0DE:ABAD1DEA in 0xC0DEBA5E.</div>
+            <div className='row'>The current request will be terminated.</div>
+            <div className='row'>&nbsp;</div>
+            <div className='row'>* Press any key to return to the previous page.</div>
+            <div className='row'>* Press CTRL+ALT+DEL to restart your computer. You will</div>
+            <div className='row'>&nbsp;&nbsp;lose any unsaved information in all applications.</div>
+            <div className='row'>&nbsp;</div>
+            <div className='centered'>Press any key to continue...<span className='blink'>&#9608;</span></div>
+            <input id='run' autoFocus onBlur={() => this.run()} onKeyPress={() => this.run()}></input>
         </div>
       </div>
     );
