c7962552dc620a304e94799b3cc29ecbf10d0be5
TheBotlyNoob <thebotlynoob@gmail.com>
2021-05-21 20:00:01 -0400

    dark mode

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,9 +2,9 @@
 
 export default function MainPage () {
   return (
-    <div id='main' className='dark:text-white'>
+    <div id='main'>
       <footer>
-        <span>This Website Is Made In: <a href="https://reactjs.org/"><button>ReactJS</button></a>, And Is Open Source On: <a href="https://github.com/TheBotlyNoob/TheBotlyNoob.github.io/">GitHub</a>!</span>
+        <span>This Website Is Made In: <a href="https://reactjs.org/">ReactJS</a>, And Is Open Source On: <a href="https://github.com/TheBotlyNoob/TheBotlyNoob.github.io/">GitHub</a>!</span>
       </footer>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extends: {}
   },
   plugins: []
 }
