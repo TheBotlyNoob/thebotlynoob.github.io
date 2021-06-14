b47112ff388fdb27060e1ad886c50a2d1ce775f0
TheBotlyNoob <thebotlynoob@gmail.com>
2021-06-14 18:32:38 -0400

    fix blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -33,10 +33,14 @@ export default class Blog extends React.Component {
                   </div>
                   <div className='flex-grow pl-6'>
                     <h2 className='tracking-widest text-xs font-medium text-indigo-500 mb-1'>{i.cat}</h2>
-                    <Link to={i.slug}>
-                      <h1 className='text-xl font-medium text-gray-900 mb-3'>{i.title}</h1>
-                      <p className='leading-relaxed mb-5'>{i.desc}</p>
-                    </Link>
+                    <h1 className='text-xl font-medium text-gray-900 mb-3'>{i.title}</h1>
+                    <p className='leading-relaxed'>{i.desc}</p>
+                    <Link to={i.slug} class="text-indigo-500 inline-flex items-center mt-4 mb-5">Learn More
+                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
                   </div>
                 </div>
               </div>
