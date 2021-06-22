c72339b1e2d47dd45a53c1ecfe9a70456ef1b8d4
TheBotlyNoob <thebotlynoob@gmail.com>
2021-06-21 20:29:24 -0400

    Hola

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -35,8 +35,8 @@ export default class Blog extends React.Component {
                   <h2 className='category'>{i.cat}</h2>
                   <h1 className='title'>{i.title}</h1>
                   <p className='desc'>{i.desc}</p>
-                  <Link to={i.slug} class='learn-more'>Learn More
-                  <svg class='w-4 h-4 ml-2' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+                  <Link to={i.slug} className='learn-more'>Learn More
+                  <svg className='arrow' viewBox='0 0 24 24' stroke='currentColor' strokeWidth='2' fill='none' strokeLinecap='round' strokeLinejoin='round'>
                     <path d='M5 12h14'></path>
                     <path d='M12 5l7 7-7 7'></path>
                   </svg>
