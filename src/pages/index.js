600e3ecbbc24b44de95a7c8bbc34c52205391160
Mahir Molai <mahir@molai.dev>
2021-06-23 04:39:22 +0500

    Add a homepage
    This homepage shares a little about you and gives an overview about your projects, however, i'd recommend a seperate projects page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'gatsby';
 import { Seo } from '../components';
 import { Layout } from '../components';
 import { Octokit, log } from '../utils';
@@ -13,6 +14,16 @@ export default function MainPage () {
   return (
     <Layout>
       <Seo title='Home'/>
+        <h1>
+          Hi, I'm Jay!
+        </h1>
+        <h3>
+          I am a passionate developer, I like to use web technologies like React, CSS, And JavaScript. I've Worked On Many Projects, Here are my current ones:
+        </h3>
+        <ul>
+          <li>Freedom App - A Social Media Platform - <Link to="https://git.opensrc.services/molai.dev/freedom-app">Git Repository</Link></li>
+          <li>MolaiBOT - A Discord Bot - <Link to="https://github.com/mtgsquad/molaibot">Git Repository</Link></li>
+        </ul>
     </Layout>
   );
 };
