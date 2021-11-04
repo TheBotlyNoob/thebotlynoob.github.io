824286efacec0643bc7767cad72d817d441bd7f1
Jay Jackson <thebotlynoob@gmail.com>
2021-11-04 14:47:21 -0400

    Update config.sh

--- static/config.sh
+++ static/config.sh
@@ -1,3 +1,3 @@
-## Setup Arch, More Info at https://github.com/TheBotlyNoob/My-Arch-Config
+## Setup WSL, More Info at https://github.com/TheBotlyNoob/My-WSL-Config
 
 curl -H 'Cache-Control: no-cache' -sSL "https://raw.githubusercontent.com/TheBotlyNoob/My-WSL-Config/master/install.sh" | bash
