104eb593d14b3b30e554484825e86f29acbb340e
TheBotlyNoob <jj@jj.is-a.dev>
2021-12-13 21:40:38 -0500

    fix block

--- src/ts/playit-config-generator.ts
+++ src/ts/playit-config-generator.ts
@@ -2,4 +2,4 @@ import j2t from "json2toml";
 
 let json = { thing: "otherthing" };
 
-document.getElementById("config-output").innerHTML = j2t(json) as string;
+document.getElementById("config-output").innerHTML = j2t(json);
