167ee6a43cbe8b9971608d4f825702c800c29971
TheBotlyNoob <jj@jj.is-a.dev>
2021-12-15 15:50:51 -0500

    syntax highlighting!

--- src/ts/playit-config-generator.ts
+++ src/ts/playit-config-generator.ts
@@ -1,7 +1,1 @@
 import j2t from "json2toml";
-
-setTimeout(() => {
-  let json = { thing: "otherthing" };
-
-  document.getElementById("config-output").innerHTML = j2t(json);
-}, 5000);
--- src/ts/prism.ts
+++ src/ts/prism.ts
@@ -4,50 +4,51 @@ import "prismjs/themes/prism-tomorrow.css";
 // Plugins
 import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
 import "prismjs/plugins/autolinker/prism-autolinker";
+import "prismjs/plugins/autolinker/prism-autolinker.css";
 import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/toolbar/prism-toolbar.css";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 import "prismjs/plugins/show-language/prism-show-language";
+import "prismjs/plugins/line-numbers/prism-line-numbers";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
-// Languages
+// Extra languages
 import "prismjs/components/prism-toml";
 
 Prism.highlightAll();
 
 Array.from(
   document.querySelectorAll('code[class*="language-"], pre[class*="language-"]')
-).map((element) => {
-  let handler = throttled(1000, () => Prism.highlightElement(element));
-  
-  document.addEventListener("DOMSubtreeModified", handler);
-});
+).map((element) =>
+  createObserver(element, () => Prism.highlightElement(element))
+);
 
-// function createListener(
-//   type: string,
-//   element: Element,
-//   callback: (ev: Event) => any,
-//   timeout: number
-// ) {
-//   element.addEventListener(type, (ev) => {
-//     element.removeEventListener(type, callback);
-//     console.log('removed callback!');
+createObserver(document.body, (mutations) =>
+  mutations.map((mutation) =>
+    Array.from(mutation.addedNodes).map((node) =>
+      Array.from((node as Element).classList || []).map((element_class) => {
+        if (element_class.startsWith("language-")) {
+          Prism.highlightElement(node as Element);
 
-//     console.log('started callback')
-//     callback(ev);
+          createObserver(node as Element, () =>
+            Prism.highlightElement(node as Element)
+          );
+        }
+      })
+    )
+  )
+);
 
-//     console.log(
-//       'setting the timeout');
-//     setTimeout(() => {console.log('starting the listener again');createListener(type, element, callback, timeout)}, timeout);
-//   });
-// }
+function createObserver(
+  element: Element,
+  callback: MutationCallback,
+  timeout: number = 500
+) {
+  new MutationObserver((mutations, observer) => {
+    observer.disconnect();
 
-function throttled(delay: number, fn: Function) {
-  let lastCall = 0;
-  return function (...args: any[]) {
-    const now = new Date().getTime();
-    if (now - lastCall < delay) {
-      return;
-    }
-    lastCall = now;
-    return fn(...args);
-  };
+    callback(mutations, observer);
+
+    setTimeout(() => createObserver(element, callback, timeout), timeout);
+  }).observe(element, { subtree: true, childList: true });
 }
