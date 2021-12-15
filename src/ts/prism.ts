cc1d03604cbdde3edc8d7cfa9aaded72c7e8c109
Jay Jackson <jj@jj.is-a.dev>
2021-12-15 19:07:04 +0000

    :(

--- src/ts/prism.ts
+++ src/ts/prism.ts
@@ -1,37 +1,53 @@
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
+
+// Plugins
 import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
 import "prismjs/plugins/autolinker/prism-autolinker";
 import "prismjs/plugins/toolbar/prism-toolbar";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 import "prismjs/plugins/show-language/prism-show-language";
-import components from "prismjs/components";
 
-const languages = Object.keys(components);
-
-Array.from(
-  document.querySelectorAll('code[class*="language-"], pre[class*="language-"]')
-).map((element) =>
-  element.addEventListener(
-    "DOMSubtreeModified",
-    throttle(() => Prism.highlightElement(element), 1000)
-  )
-);
+// Languages
+import "prismjs/components/prism-toml";
 
 Prism.highlightAll();
 
-function throttle(callback: Function, limit: number) {
-  var waiting = false; // Initially, we're not waiting
-  return () => {
-    // We return a throttled function
-    if (!waiting) {
-      // If we're not waiting
-      callback(); // Execute users function
-      waiting = true; // Prevent future invocations
-      setTimeout(() => {
-        // After a period of time
-        waiting = false; // And allow future invocations
-      }, limit);
+Array.from(
+  document.querySelectorAll('code[class*="language-"], pre[class*="language-"]')
+).map((element) => {
+  let handler = throttled(1000, () => Prism.highlightElement(element));
+  
+  document.addEventListener("DOMSubtreeModified", handler);
+});
+
+// function createListener(
+//   type: string,
+//   element: Element,
+//   callback: (ev: Event) => any,
+//   timeout: number
+// ) {
+//   element.addEventListener(type, (ev) => {
+//     element.removeEventListener(type, callback);
+//     console.log('removed callback!');
+
+//     console.log('started callback')
+//     callback(ev);
+
+//     console.log(
+//       'setting the timeout');
+//     setTimeout(() => {console.log('starting the listener again');createListener(type, element, callback, timeout)}, timeout);
+//   });
+// }
+
+function throttled(delay: number, fn: Function) {
+  let lastCall = 0;
+  return function (...args: any[]) {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
     }
+    lastCall = now;
+    return fn(...args);
   };
 }
