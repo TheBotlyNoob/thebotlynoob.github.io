fb17c3a226871733c247d9a7375cef851b5abe5c
Jay Jackson <jj@jj.is-a.dev>
2022-09-21 18:37:06 +0000

    working on

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,8 @@
-// vite define config
-import { defineConfig } from "vite"
-// vite plugin
-import UnoCSS from "unocss/vite"
-import { presetTagify, presetIcons, extractorSvelte } from "unocss"
-import { VitePWA } from "vite-plugin-pwa"
-import { sveltekit } from "@sveltejs/kit/vite"
-// postcss & tailwindcss
-import TailwindCSS from "tailwindcss"
-import tailwindConfig from "./tailwind.config"
-import autoprefixer from "autoprefixer"
-import cssnano from "cssnano"
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-  envPrefix: "URARA_",
-  css: {
-    postcss: {
-      plugins: [
-        TailwindCSS(tailwindConfig),
-        autoprefixer(),
-        ...(process.env.NODE_ENV === "production"
-          ? [
-              cssnano({
-                preset: ["default", { discardComments: { removeAll: true } }]
-              })
-            ]
-          : [])
-      ]
-    }
-  },
-  plugins: [
-    UnoCSS({
-      include: [/\.svelte$/, /\.md?$/, /\.ts$/],
-      extractors: [extractorSvelte],
-      presets: [
-        presetTagify({
-          extraProperties: (matched: string) => (matched.startsWith("i-") ? { display: "inline-block" } : {})
-        }),
-        presetIcons({ scale: 1.5 })
-      ]
-    }),
-    VitePWA({
-      srcDir: "./build",
-      outDir: "./.svelte-kit/output/client",
-      registerType: "autoUpdate",
-      scope: "/",
-      base: "/"
-    }),
-    sveltekit()
-  ]
-})
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
+
+export default config;
