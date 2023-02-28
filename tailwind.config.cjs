2c6e59a3560b94a3b05952163106feab94c84221
Jay Jackson <jj@jj.is-a.dev>
2023-02-28 18:17:38 -0500

    theme switcher

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "media",
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {}
