42d728bb30c1fb9b294c3dc931e93b768e3d7b00
Jay D Jackson <jj@jj.is-a.dev>
2023-02-28 21:49:40 +0000

    move back to astro

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Hello 👋! I'm Jay.</h1>
-<p>
-    Knower of <i style="color: #54b691">Cybersecurity</i>, <i style="color: #ed4800">Rust</i>,
-    <i style="color: yellow">JavaScript</i>, <i style="color: #498fec">TypeScript</i>,
-    <i style="color: #86d4ff">Go</i>,
-    <i style="color: #ff6b00">Svelte</i>,
-    <i style="color: #73d2ff">React</i>, and <i style="color: #5cd390">Vue</i>
-</p>
